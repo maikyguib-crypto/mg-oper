daa55ac4e23ef62e0fc8e5eb36e148826ca84ae5
maikyguib-crypto <maikyguib@gmail.com>
2026-09-14 19:36:29 -0300

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,3 @@
+import './globals.css'
+export const metadata={title:'MG Oper',description:'Gestão diária de equipes e operações'}
+export default function RootLayout({children}){return <html lang="pt-BR"><body>{children}</body></html>}
